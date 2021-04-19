6b5dd464ce2f5cf3085c0c4ffed4b5abf54cb4e6
Perfect Imhetsike Igbadumhe <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-04-19 23:38:27 +0100

    client register page

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import RegisterForm from '../components/RegisterForm'
+import axios from 'axios'
+
 
 const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit= async (e)=> {
+        e.preventDefault();
+        try{
+            const res = await axios.post(`http://localhost:8000/api/register`, {
+                name,
+                email,
+                password
+            });
+            console.log(`REGISTER USER ${res}`)
+        }catch(err){
+            console.log(`error USER ===> ${err}`)
+        }
+    }
+
+    
+
     return (
-        <div className="container-fluid h1 p-5 text-center">
-            Register Page
+        <div>
+            <div className="container-fluid bg-secondary text-center p-5">
+                <h1>Register</h1>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <RegisterForm 
+                            handleSubmit={handleSubmit}
+                            name={name}
+                            setName={setName}
+                            email={email}
+                            setEmail = {setEmail}
+                            password={password}
+                            setPassword = {setPassword}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const RegisterForm = ({handleSubmit, name, setName, email, setEmail, password, setPassword}) => {
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="mt-5">
+            <div className="form-group">
+                <label className="form-label">Name</label>
+                <input 
+                    type="text"
+                    placeholder="Enter name"
+                    className="form-control mb-2"
+                    value={name}
+                    onChange={e=> setName(e.target.value)}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="form-label">Email</label>
+                <input 
+                    type="email"
+                    placeholder="Enter Email"
+                    className="form-control mb-2"
+                    value={email}
+                    onChange={e=> setEmail(e.target.value)}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="form-label">Password</label>
+                <input 
+                    type="password"
+                    placeholder="Enter password"
+                    className="form-control mb-2"
+                    value={password}
+                    onChange={e=> setPassword(e.target.value)}
+                />
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+        </form>
+        </div>
+    )
+}
+
+export default RegisterForm
