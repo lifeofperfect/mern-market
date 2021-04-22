d64ddf97669d8da645083219fb3fa8bde21c62d6
Perfect Imhetsike Igbadumhe <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-04-22 14:46:36 +0100

    added toast notification

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import RegisterForm from '../components/RegisterForm'
-import axios from 'axios'
+import axios from 'axios';
+
+//toastify
+import {toast} from 'react-toastify'
 
 
-const Register = () => {
+const Register = ({history}) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    //console.log("history", history)
 
     const handleSubmit= async (e)=> {
         e.preventDefault();
@@ -17,8 +22,15 @@ const Register = () => {
                 password
             });
             console.log(`REGISTER USER ${res}`)
+            history.push(`/login`)
+            toast.success("Registeration succesfull return to login")
+            
         }catch(err){
             console.log(`error USER ===> ${err}`)
+
+            if(err.response.status === 400)
+                toast.error(err.response.data)
+            
         }
     }
 
@@ -30,6 +42,7 @@ const Register = () => {
                 <h1>Register</h1>
             </div>
 
+            
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
